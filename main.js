5b835845736bff8b20dca3131c9bd20d0bca4a5a
Igor Grafov <grafov2711@gmail.com>
2024-03-11 19:37:47 +0300

    finish

--- main.js
+++ main.js
@@ -27,12 +27,27 @@ document.addEventListener("DOMContentLoaded", function () {
       currentScreen.textContent = currentValue;
     })
   );
-  clear.addEventListener("click", function (e) {
+  clear.addEventListener("click", function () {
     previousValue = "";
     currentValue = "";
     operator = "";
     previousScreen.textContent = currentValue;
     currentScreen.textContent = currentValue;
+  });
+
+  equal.addEventListener("click", function (e) {
+    if (currentValue != "" && previousValue != "") {
+      calculate(e);
+      previousScreen.textContent = "";
+      if (previousValue.length <= 5) {
+        currentScreen.textContent = previousValue;
+      } else {
+        currentScreen.textContent = previousValue.slice(0, 5) + "...";
+      }
+    }
+  });
+  decimal.addEventListener("click", function () {
+    addDecimal();
   });
 });
 
@@ -45,4 +60,31 @@ function handleOperator(op) {
   operator = op;
   previousValue = currentValue;
   currentValue = "";
+}
+
+function calculate() {
+  previousValue = Number(previousValue);
+  currentValue = Number(currentValue);
+
+  if (operator === "+") {
+    previousValue += currentValue;
+  } else if (operator === "-") {
+    previousValue -= currentValue;
+  } else if (operator === "x") {
+    previousValue *= currentValue;
+  } else {
+    previousValue /= currentValue;
+  }
+  previousValue = roundNumber(previousValue);
+  previousValue = previousValue.toString();
+  currentValue = currentValue.toString();
+}
+
+function roundNumber(num) {
+  return Math.round(num * 1000) / 1000;
+}
+function addDecimal() {
+  if (!currentValue.includes(".")) {
+    currentValue += ".";
+  }
 }
